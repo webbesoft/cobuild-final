2a67a80528ab93a557bcc89a7bd9edeee9e47ee5
Tawanda <ejmunongo@gmail.com>
2025-01-21 18:01:00 +0200

    add tailwind css support on frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        lightBg: "#f9feff",
+        primary: "#0A363C",
+        primaryLighter: "#125F69",
+        accent: "#c19e86",
+        secondary: "#f88687",
+        text: "#021c20",
+        textLight: "#f9feff",
+        darkened: "#030f11",
+      },
+    },
+  },
+  plugins: [],
+};
